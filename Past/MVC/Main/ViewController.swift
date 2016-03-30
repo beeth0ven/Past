dad47ac5b09fea2d98fa97d93594b8808181495a
beeth0ven <beeth0vendev@163.com>
2016-03-30 09:55:43 +0800

    Init

---
+++ Past/MVC/Main/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Past
+//
+//  Created by luojie on 16/3/30.
+//  Copyright © 2016年 LuoJie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+
+platform :ios, '9.0'
+use_frameworks!
+
+target 'Past' do
+    pod 'RxSwift',    '~> 2.0'
+    pod 'RxCocoa',    '~> 2.0'
+    pod 'RxBlocking', '~> 2.0'
+    pod 'RxTests',    '~> 2.0'
+end
+
