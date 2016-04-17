e1a41dcbf08b1cf302566fbd08bd2bc4c9902848
beeth0ven <beeth0vendev@163.com>
2016-04-17 18:43:02 +0800

    Before add visit.

--- Past/Public/MapKit/MapKit+.swift
+++ Past/Public/MapKit/MapKit+.swift
@@ -6,11 +6,30 @@
 //  Copyright © 2016年 LuoJie. All rights reserved.
 //
 
-import Foundation
 import MapKit
+
+// MARK: - MapKit
+
+// MARK: - CoreLocation
 
 extension CLLocationCoordinate2D: CustomStringConvertible {
     public var description: String {
         return "latitude: \(latitude) longitude:\(longitude)"
+    }
+}
+
+extension CLVisit {
+    enum Option {
+        case Arrival
+        case Departure
+    }
+    
+    var option: Option {
+        if departureDate == NSDate.distantFuture() {
+            return .Arrival
+        } else if arrivalDate == NSDate.distantPast() {
+            return .Departure
+        }
+        fatalError("Visit option should be either arrive or departure.")
     }
 }
